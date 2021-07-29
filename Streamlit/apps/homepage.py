f9dee9579dc73d66177d01c217a367a4a39d931e
Sarvagya Kumar <sarvagya20kumar@gmail.com>
2021-07-30 01:23:17 +0530

    Add files via upload

---
+++ Streamlit/apps/homepage.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 29 17:42:01 2021
+
+"""
+import streamlit as st
+def app():
+    st.title('Homepage')
+    
+    st.subheader('Here you can browse the sidebar for using various tools such as')
+    st.markdown('1. CT scan detector')
+    st.markdown('2. Live sentiment analysis model')
+    st.markdown('3. Real time vaccine tracker')
