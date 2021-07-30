e90504f699dc82fa46610adf92b893361e4fb59b
suriya-it19 <54205777+suriya-it19@users.noreply.github.com>
2021-07-30 13:53:04 +0530

    added detection app

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -1,13 +1,14 @@
 import streamlit as st
 from multiapp import MultiApp
 from apps import homepage
+from apps.CT_Image_Detector.main import detect
 
 def main():
 
     app = MultiApp()
     st.markdown("""# Welcome to the application for Socio-Economic Impact of Covid19""")
     app.add_app("Homepage",homepage.app)
-    #app.add_app("
+    app.add_app("CT Scan Detector",detect)
     app.run()
 
 if __name__ == "__main__":
