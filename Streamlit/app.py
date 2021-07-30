464e100af246d5d3cfaab85d94ba1bf79d617513
suriya-it19 <54205777+suriya-it19@users.noreply.github.com>
2021-07-30 15:50:43 +0530

    added sentiment

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from multiapp import MultiApp
 from apps import homepage
-from apps.CT_Image_Detector.main import detect
+from apps.Classification_modelling.CT_Image_Detector.main import detect
+from apps.Sentiment_Analysis.sentimentinference import detect1
 
 def main():
 
@@ -9,6 +10,7 @@ def main():
     st.markdown("""# Welcome to the application for Socio-Economic Impact of Covid19""")
     app.add_app("Homepage",homepage.app)
     app.add_app("CT Scan Detector",detect)
+    app.add_app("Sentiment analysis",detect1)
     app.run()
 
 if __name__ == "__main__":
