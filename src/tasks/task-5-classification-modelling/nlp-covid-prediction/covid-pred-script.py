b1205fb133eb568719ef3236f65612ca1378f006
Ritvik Nimmagadda <47041400+RitvikN18@users.noreply.github.com>
2021-07-31 15:38:26 +0530

    Create covid-pred-script.py

---
+++ src/tasks/task-5-classification-modelling/nlp-covid-prediction/covid-pred-script.py
@@ -0,0 +1,15 @@
+def predictCovid(inputArray):
+    count0 = 0
+    count1 = 0
+    for i in range(1, 9):
+        model_load = xgb.XGBClassifier()
+        model_load.load_model("model" + str(i) + ".json")
+        result = model_load.predict(inputArray.reshape((1, 8)))
+        if result[0] == 1:
+            count1 += 1
+        else:
+            count0 += 1
+    if count1 >= count0:
+        return True
+    else:
+        return False
