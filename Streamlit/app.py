547103fdfd3e61a6c0ac853b0f043a19bab0fbfe
suriya-it19 <54205777+suriya-it19@users.noreply.github.com>
2021-08-12 12:51:16 +0530

    added time series

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -4,15 +4,17 @@ from apps import homepage
 from apps.Classification_modelling.CT_Image_Detector.main import detect
 from apps.Sentiment_Analysis.sentimentinference import detect1
 from apps.Classification_modelling.Covid_Symptoms_Prediction.main import detect2
+from apps.Time_Series.forecast import detectf
 
 def main():
 
     app = MultiApp()
-    st.markdown("""# Welcome to the application for Socio-Economic Impact of Covid19""")
+    st.markdown("""# Socio-Economic Impact of Covid19""")
     app.add_app("Homepage",homepage.app)
     app.add_app("CT Scan Detector",detect)
     app.add_app("Sentiment analysis",detect1)
     app.add_app("Covid symptoms",detect2)
+    app.add_app("Forecast",detectf)
     app.run()
 
 if __name__ == "__main__":
