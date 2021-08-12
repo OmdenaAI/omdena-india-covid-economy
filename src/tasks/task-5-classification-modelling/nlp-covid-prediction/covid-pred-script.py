ddc363d075dcef5dd68d440b75b007b656cf1259
Ritvik Nimmagadda <47041400+RitvikN18@users.noreply.github.com>
2021-08-12 11:48:13 +0530

    Update covid-pred-script.py

--- src/tasks/task-5-classification-modelling/nlp-covid-prediction/covid-pred-script.py
+++ src/tasks/task-5-classification-modelling/nlp-covid-prediction/covid-pred-script.py
@@ -4,15 +4,19 @@ import xgboost as xgb
 def predictCovid(inputArray):
     count0 = 0
     count1 = 0
+    prob = 0
     for i in range(1, 9):
         model_load = xgb.XGBClassifier()
         model_load.load_model("model" + str(i) + ".json")
         result = model_load.predict(inputArray.reshape((1, 8)))
+        tempProb = model_load.predict_proba(inputArray.reshape((1, 8)))
+        prob += tempProb[0][1]
         if result[0] == 1:
             count1 += 1
         else:
             count0 += 1
+    prob = prob/8
     if count1 >= count0:
-        return True
+        return True, prob 
     else:
-        return False
+        return False, prob
