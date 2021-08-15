ea1f152c38dea9890947b3101584dd610824e6f4
suriya-it19 <54205777+suriya-it19@users.noreply.github.com>
2021-08-15 18:24:20 +0530

    BG color update

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -2,17 +2,17 @@ import streamlit as st
 from multiapp import MultiApp
 from apps import homepage
 from apps.Classification_modelling.CT_Image_Detector.main import detect
-from apps.Sentiment_Analysis.sentimentinference import detect1
+# from apps.Sentiment_Analysis.sentimentinference import detect1
 from apps.Classification_modelling.Covid_Symptoms_Prediction.main import detect2
 from apps.Time_Series.forecast import detectf
 
 def main():
-
+    st.sidebar.image("logo.jfif", use_column_width=True)
     app = MultiApp()
     st.markdown("""# Socio-Economic Impact of Covid19""")
     app.add_app("Homepage",homepage.app)
     app.add_app("CT Scan Detector",detect)
-    app.add_app("Sentiment analysis",detect1)
+    # app.add_app("Sentiment analysis",detect1)
     app.add_app("Covid symptoms",detect2)
     app.add_app("Forecast",detectf)
     app.run()
